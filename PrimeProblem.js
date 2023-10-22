2a1abaea3553b94c2a08bb69910237fbeb00edc3
Bhanupratapsingh21 <bhanupss137@gmail.com>
2023-10-22 21:22:39 +0530

    m2codefix

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -1,16 +1,15 @@
 function check_prime(num) {
   let count = 0;
-  for (let i = 2; num < i; i++) {
-    if (num % i === 1) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
       count++;
       break;
     }
   }
-  if (count === 1 && num > 1) {
+  if (count === 0 && num > 1) {
     console.log(num, "is a prime number");
   } else {
     console.log(num, "is not a prime number");
   }
 }
-check_prime(2);
-
+check_prime(9);
